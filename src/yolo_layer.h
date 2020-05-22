154e2acde209c4e00420a0021a474890da05b6af
Moonsoo Ra <ravicmoon@gmail.com>
2020-05-22 19:00:53 +0900

    update: missed changes

--- src/yolo_layer.h
+++ src/yolo_layer.h
@@ -13,7 +13,7 @@ layer make_yolo_layer(int batch, int w, int h, int n, int total, int* mask,
 void forward_yolo_layer(const layer l, NetworkState state);
 void backward_yolo_layer(const layer l, NetworkState state);
 void resize_yolo_layer(layer* l, int w, int h);
-int yolo_num_detections(layer l, float thresh);
+int YoloNumDetections(layer l, float thresh);
 int yolo_num_detections_batch(layer l, float thresh, int batch);
 int get_yolo_detections(layer l, int w, int h, int netw, int neth, float thresh,
     int* map, int relative, Detection* dets, int letter);
