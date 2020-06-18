ace91391661e97699e882841ec95a0f6437fc7a4
Moonsoo Ra <ravicmoon@gmail.com>
2020-06-18 18:33:34 +0900

    refactor: make visualize.h as public

--- src/visualize.h
+++ src/visualize.h
@@ -4,15 +4,16 @@
 
 #include "box.h"
 #include "image.h"
+#include "libapi.h"
 #include "option_list.h"
 
-void Mat2Image(cv::Mat const& mat, Image* image);
-void DrawYoloDetections(cv::Mat& img, Detection* dets, int num_boxes,
+LIB_API void Mat2Image(cv::Mat const& mat, Image* image);
+LIB_API void DrawYoloDetections(cv::Mat& img, Detection* dets, int num_boxes,
     float thresh, Metadata const& md);
 
-cv::Mat DrawLossGraphBg(
+LIB_API cv::Mat DrawLossGraphBg(
     int max_iter, float max_loss, int num_lines, int img_size);
-void DrawLossGraph(cv::Mat const& bg, std::vector<int> const& iter,
+LIB_API void DrawLossGraph(cv::Mat const& bg, std::vector<int> const& iter,
     std::vector<float> const& avg_loss, std::vector<int> const& iter_map,
     std::vector<float> const& map, int max_iter, float max_loss,
     double time_remaining);
